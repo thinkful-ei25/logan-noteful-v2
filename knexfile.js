519bb6a7163ba157af5a80ad85361b857915ec20
Logan Wang <loganxwang@hotmail.com>
2018-10-16 12:52:41 -0700

    CRUD queries

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 'use strict';
-
+//'postgresql://postgres:Qwerty54!@localhost/noteful-app';
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/noteful-app',
+    connection: process.env.DATABASE_URL || 'postgresql://postgres:Qwerty54!@localhost/noteful-app',
     debug: true, // http://knexjs.org/#Installation-debug
     pool: { min: 1, max: 2 }
   },
--- scratch/queries.js
+++ scratch/queries.js
@@ -2,6 +2,8 @@
 
 const knex = require('../knex');
 
+// Get All Notes accepts a searchTerm and finds notes with 
+// titles which contain the term. It returns an array of objects.
 let searchTerm = 'gaga';
 knex
   .select('notes.id', 'title', 'content')
@@ -18,3 +20,45 @@ knex
   .catch(err => {
     console.error(err);
   });
+
+// Get Note By Id accepts an ID. It returns the note as an object not an array
+let noteId = '1004';
+knex('notes')
+  .returning(['id', 'title', 'content'])
+  .where('id', noteId)
+  .then(results => console.log(results[0]))
+  .catch(err => {
+    console.error(err);
+  });
+
+// Update Note By Id accepts an ID and an object with the desired updates. 
+// It returns the updated note as an object
+knex('notes')
+  .returning(['id', 'title', 'content'])
+  .where('id',noteId)
+  .update({title:'CATS CATS CATS DOGS CATS', content:'DOGS DOGS DOGS DOGS'})
+  .then(results => console.log(results[0]))
+  .catch(err => {
+    console.error(err);
+  });
+
+// Create a Note accepts an object with the note properties and inserts it in the DB. 
+// It returns the new note (including the new id) as an object.
+knex('notes')
+  .returning(['id', 'title', 'content'])
+  .insert({title: 'DOGGS', content:'DOgs > cats'})
+  .then(results => console.log(results[0]))
+  .catch(err => {
+    console.error(err);
+  });
+// Delete Note By Id accepts an ID and deletes the note from the DB.
+knex('notes')
+  .where('id', 1046)
+  .del()
+  .then(results => console.log(results[0]))
+  .catch(err => {
+    console.error(err);
+  });
+
+knex('notes')
+  .then(results => console.log(results));
